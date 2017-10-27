2ced1a319648743f91037f0d58048b36ffb431ca
vineethkasula <nikkyvineeth@gmail.com>
2017-10-27 17:15:55 +0530

    Updated

---
+++ app/src/main/java/com/example/vineeth/medizyl12/Pre.java
@@ -0,0 +1,13 @@
+package com.example.vineeth.medizyl12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre);
+    }
+}
