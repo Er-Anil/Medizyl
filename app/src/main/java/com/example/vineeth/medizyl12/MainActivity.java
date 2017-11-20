41e2a3e7a05ffda1a0e0a5fab0b06a7f422fbf6e
vineethkasula <nikkyvineeth@gmail.com>
2017-11-20 15:27:45 +0530

    prefinal update1

--- app/src/main/java/com/example/vineeth/medizyl12/MainActivity.java
+++ app/src/main/java/com/example/vineeth/medizyl12/MainActivity.java
@@ -6,10 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button b1,b2;
+    ImageView iv1,iv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
         b1 = (Button) findViewById(R.id.p1);
         b2 = (Button) findViewById(R.id.m1);
+        iv2 = (ImageView) findViewById(R.id.imageView6); 
+        iv1 = (ImageView) findViewById(R.id.imageView5);
     }
 
     public void pre(View view)
